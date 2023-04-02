edbffca709c805a190c5f5b8593a86d09f17f422
Silvia Milanes <silviamilanes@yahoo.com>
2023-04-02 15:10:46 -0400

    finished sprint 3

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -2,57 +2,6 @@ const apiKey = `?api_key=5a3fa11d-8c58-4c11-bd97-4c292d09c0d6`;
 const apiBaseUrl = 'https://project-1-api.herokuapp.com/';
 
 let shows = []
-    // {   dateTitle: "DATE",
-    //     date: 'Mon Sept 06 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'Madison Square Garden',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-        
-    // },
-    // {
-    //     dateTitle: "DATE",
-    //     date: 'Tue Sept 21 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'Pier 3 East',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-    // },
-    // {
-    //     dateTitle: "DATE",
-    //     date: 'Fri Oct 15 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'View Lounge',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-    // },
-    // {
-    //     dateTitle: "DATE",
-    //     date: 'Sat Nov 06 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'Hyatt Agency',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-    // },
-    // {
-    //     dateTitle: "DATE",
-    //     date: 'Fri Nov 26 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'Moscow Center',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-    // },
-    // {
-    //     dateTitle: "DATE",
-    //     date: 'Wed Dec 15 2021',
-    //     venueTitle: "VENUE",
-    //     venue: 'Press Club',
-    //     locationTitle: "LOCATION",
-    //     location: 'San Francisco, CA'
-    // },
-// ];
-
-// console.log(shows);
 
 axios
     .get (`${apiBaseUrl}showdates${apiKey}`) 
@@ -65,15 +14,12 @@ axios
     console.error(error);
     });
 
-
 // Function to create show cards
 const showSelector = document.querySelector (".shows");
 const showSection = document.createElement ("section");
 showSelector.appendChild(showSection);
 
 showSection.classList.add ("showSection")
-
-
 
 function showCards(showsArray) {
     for (let i = 0; i < showsArray.length; i++) {
@@ -83,9 +29,8 @@ function showCards(showsArray) {
         const dateElement = document.createElement('p');
         dateElement.classList.add('date');
         const dateObj = new Date(showsArray[i].date);
-    const dateString = `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`;
-    dateElement.textContent = dateString;
-
+        const dateString = `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`;
+        dateElement.textContent = dateString;
 
         const venueElement = document.createElement('p');
         venueElement.classList.add('venue');
@@ -122,7 +67,6 @@ function showCards(showsArray) {
         showSection.appendChild(card);
     } }
 
-    // showCards(shows);
 
 
 
